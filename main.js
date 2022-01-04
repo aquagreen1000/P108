08c42c045c54eca7dfccb595d6391a6ca74ece09
aquagreen1000 <87820267+aquagreen1000@users.noreply.github.com>
2022-01-03 20:55:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+var dog=0;
+var cat=0;
+function gotResults(error, results) {
+if (error) {
+    console.error(error);
+} else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of '+
+    results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected dog - '+dog+'detected cat -'+cat;
+    document.getElementById("result_label").style.color = "rgb("
+    +random_number_r+","+random_number_g+","+random_number_b+")";
+    document.getElementById("result_count").style.color = "rgb("
+    +random_number_r+","+random_number_g+","+random_number_b+")";
+
+img = document.getElementById('animal_img');
+    
+    if (results[0].label == "Bark") {
+    img.src = 'https://media2.giphy.com/media/3ndAvMC5LFPNMCzq7m/giphy-downsized-large.gif';  
+    dog = dog + 1;
+    } else if (results[0].label == "Meow") {
+        img.src = 'https://media4.giphy.com/media/l3q2up9FZPFncIt1e/200.gif';
+        cat = cat + 1;
+    }else{img.src = 'https://w7.pngwing.com/pngs/958/129/png-transparent-hearing-art-hearing-sound-sense-human-body-cartoon-ear-cartoon-character-text-hand.png';
+    }
+  }
+}
+
+
+
